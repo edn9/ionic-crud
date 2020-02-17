8b5d6aa9e0643fc0b9d32eb8b06ea4e2d2c06988
edn9 <eduardohenriquen9@outlook.com>
2020-02-16 23:29:00 -0300

    Crud Ionic PHP Mysql Project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  email: string = "";
+  password: string = "";
+  login: any = {};
+  constructor(public navCtrl: NavController, private http: HttpClient) { }
 
-  constructor() {}
+  onSubmit() {
+    this.login = this.email, this.password;
+    console.log(this.login);
+    this.http.post('api/controller/AuthController.php', this.login).subscribe(data => {
+      console.log(data);
+      let result = JSON.parse(data["_body"]);
+      if (result.status == "success") {
+        console.log('Sucesso!')
+      }
+      else {
+        console.log('Algo deu errado!')
+      }
+    }, err => {
+      console.log(err);
+    })
+  }
 
+  register() {
+    this.navCtrl.navigateForward('/register');
+  }
 }
---
+++ src/app/register/register.page.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController, ToastController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+  register: any = {};
+  toast: any;
+  constructor(public navCtrl: NavController, private http: HttpClient, public toastController: ToastController) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    this.register.action = "insert";
+    this.http.post("http://localhost/api/controller/Register.php", this.register).subscribe(data => {
+      if (data['status'] == "success") {
+        var message = "Registrado com sucesso!";
+        console.log(message);
+        this.showToast(message);
+      } else {
+        var message = data['status'];
+        console.log(message);
+        this.showToast(message);
+      }
+    }, err => {
+      console.log(err);
+      this.showToast(err);
+    })
+  }
+
+  async showToast(message) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'middle',
+      buttons: [
+        {
+          text: 'Ok',
+          role: 'cancel'
+        }
+      ]
+    });
+    toast.present();
+  }
+
+  back() {
+    this.navCtrl.back();
+  }
+}
